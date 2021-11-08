ae33343dd070de820db8714ee41fae1321855918
Nuria788 <200093@iessierradeguara.com>
2021-11-08 18:58:44 +0100

    cambio de carpetas

---
+++ db1.php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <from action="ejercicio8.php" method='get'>
+        <p>numero de divs</p>
+        <input type="text"name="numero">
+
+        <p> Texto dentro de divs<input</p>
+        <input type="text"name="texto">
+        
+        <p>color</p>
+         <input type="text"name="color">
+
+</body>
+</html>
